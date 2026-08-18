41a64ce410640b4e90c2206cf1aab94619c37e3b
Yongtae999 <yongtae999@example.com>
2026-08-18 09:33:31 +0900

    fix: Update Doowoong wetland exact center coords to User confirmed (36.836407 N, 126.196043 E) and re-anchor zones and flight presets

--- drone-map.js
+++ drone-map.js
@@ -153,10 +153,10 @@ class DroneMapController {
         break;
 
       case 'zone-1':
-        // Zone 1
+        // Zone 1: 미국수련 제거구역
         this.map.flyTo({
-          center: isDoowoong ? [126.1957, 36.8358] : [127.5705, 36.1095],
-          zoom: isDoowoong ? 17.5 : 16.2,
+          center: isDoowoong ? [126.1960, 36.8364] : [127.5705, 36.1095],
+          zoom: isDoowoong ? 17.6 : 16.2,
           pitch: 68,
           bearing: isDoowoong ? 45 : 130,
           duration: 2500,
@@ -165,10 +165,10 @@ class DroneMapController {
         break;
 
       case 'zone-2':
-        // Zone 2
+        // Zone 2: 황소개구리 포획구역
         this.map.flyTo({
-          center: isDoowoong ? [126.1965, 36.8364] : [127.5760, 36.1055],
-          zoom: isDoowoong ? 17.5 : 16.4,
+          center: isDoowoong ? [126.1968, 36.8370] : [127.5760, 36.1055],
+          zoom: isDoowoong ? 17.6 : 16.4,
           pitch: 70,
           bearing: isDoowoong ? 55 : 110,
           duration: 2500,
@@ -177,10 +177,10 @@ class DroneMapController {
         break;
 
       case 'zone-3':
-        // Zone 3
+        // Zone 3: 기타 정비구역
         this.map.flyTo({
-          center: isDoowoong ? [126.1970, 36.8370] : [127.5805, 36.1040],
-          zoom: isDoowoong ? 17.0 : 16.0,
+          center: isDoowoong ? [126.1973, 36.8376] : [127.5805, 36.1040],
+          zoom: isDoowoong ? 17.2 : 16.0,
           pitch: 65,
           bearing: isDoowoong ? 60 : 90,
           duration: 2500,
